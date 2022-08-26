890facf00cb76e1f2f3da1af1830f0d1e60a0953
Vio <tainakaviona@gmail.com>
2022-08-26 14:52:22 +0700

    error-handling-rev

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
     def new
-        # @Products = product.all
+        @product = Product.new
 
         # render json: @Products
         render "new"
@@ -8,6 +8,7 @@ class ProductsController < ApplicationController
 
     def list
         @products = Product.all
+
         render "list"
     end
 
@@ -18,19 +19,43 @@ class ProductsController < ApplicationController
 
 
     def add
-        url = "http://world.openfoodfacts.org/api/v0/product/" + params[:upc_code] + ".json"
-        response = RestClient.get(url)
-        result = JSON.parse(response.body)
         @product = Product.new
-        @product.id = result['code']
-        @product.product_name = result['product']['product_name']
-        @product.quantity = result['product']['quantity']
-        @product.brands = result['product']['brands']
-        @product.categories = result['product']['categories']
-        @product.ingredients_text = result['product']['ingredients_text']
-        @product.imageurl = result['product']['image_front_small_url']
-        @product.save
-        render "new"
+        @message = nil
+        url = "http://world.openfoodfacts.org/api/v0/product/" + params[:upc_code] + ".json"
+        begin
+            response = RestClient.get(url)
+            result = JSON.parse(response.body)
+            if(result['status']==1)
+                @product.id = result['code']
+                @product.product_name = result['product']['product_name']
+                @product.quantity = result['product']['quantity']
+                @product.brands = result['product']['brands']
+                @product.categories = result['product']['categories']
+                @product.ingredients_text = result['product']['ingredients_text']
+                @product.imageurl = result['product']['image_front_small_url']
+
+                if @product.save
+                    begin
+                        redirect_to "/list"
+                    rescue SocketError => e
+                        @message = "Something Went Wrong"
+                        render "new"
+                    end
+                    
+                else
+                    render "new"
+                end
+            else
+                @message = "No code or invalid code"
+                render "new"
+            end
+        rescue SocketError => e
+            @message = "Oops, Something Went Wrong"
+            render "new"
+        end
+        
+
+        # render "new"
 
        
         # @response = {
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+    validates :product_name, presence: true
+    validates :id, presence: true
+    validates :id, uniqueness: true
 end
